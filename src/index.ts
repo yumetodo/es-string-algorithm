a12e35866b7fbfcaaef2c4b3103cba4b589d69b4
yumetodo <yume-wikijp@live.jp>
2019-02-12 11:33:57 +0900

    refactor: use String#includes to speedup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-const findFirstOfImpl = (target: string | string[], key: string, pos = 0): number => {
+type Pred = (c: string, key: string) => boolean;
+const findFirst = (target: string | string[], key: string, pos: number, pred: Pred): number => {
   let i = 0;
   for (const t of target) {
-    if (pos <= i) {
-      for (const k of key) {
-        if (t === k) return i;
-      }
-    }
+    if (pos <= i && pred(t, key)) return i;
     ++i;
   }
   return -1;
@@ -19,9 +16,8 @@ const findFirstOfImpl = (target: string | string[], key: string, pos = 0): numbe
 
 // size_type find_first_of(std::basic_string_view<charT, traits> sv,
 //                         size_type pos = 0) const noexcept;                          // (5) C++17
-export const findFirstOf = (target: string, key: string, pos = 0): number => {
-  return findFirstOfImpl(target, key, pos);
-};
+export const findFirstOf = (target: string, key: string, pos = 0): number =>
+  findFirst(target, key, pos, (c, key) => key.includes(c));
 // size_type find_last_of(const basic_string& str, size_type pos = npos) const noexcept; // (1) C++11
 
 // size_type find_last_of(const charT* s, size_type pos, size_type n) const;             // (2)
@@ -36,6 +32,6 @@ export const findLastof = (target: string, key: string, pos = -1): number => {
   if (targetArr.length <= pos) return -1;
   targetArr.reverse();
   pos = -1 === pos ? 0 : targetArr.length - 1 - pos;
-  const re = findFirstOfImpl(targetArr, key, pos);
+  const re = findFirst(targetArr, key, pos, (c, key) => key.includes(c));
   return -1 === re ? -1 : targetArr.length - 1 - re;
 };
