24621e9cb50798d11c6ae96b010c0db00e21a597
yumetodo <yume-wikijp@live.jp>
2019-02-12 11:32:19 +0900

    feat(test): add findFirstNotOf test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { findFirstOf, findLastof } from '../src';
+import { findFirstOf, findLastof, findFirstNotOf } from '../src';
 
 describe('findFirstOf', () => {
   it('by string', () => {
@@ -42,5 +42,27 @@ describe('findLastOf', () => {
     const s = '🍣🍺';
     expect(findLastof(s, '🍺')).toEqual(1);
     expect(findLastof(s, '👩‍❤️‍💋‍👨')).toEqual(-1);
+  });
+});
+describe('findFirstNotOf', () => {
+  it('by string', () => {
+    const s = 'Hello, world. Welcome to C++ world.';
+    const str = 'world';
+    expect(findFirstNotOf(s, str)).toEqual(0);
+    expect(findFirstNotOf(s, str, 2)).toEqual(5);
+    expect(findFirstNotOf(s, str, 40)).toEqual(-1);
+    expect(findFirstNotOf(s, 'worlde,. ', 1)).toEqual(14);
+    expect(findFirstNotOf(s, 'ab')).toEqual(0);
+  });
+  it('by char', () => {
+    const s = 'arikitari na sekai datta';
+    expect(findFirstNotOf(s, 'a')).toEqual(1);
+    expect(findFirstNotOf(s, 't', 21)).toEqual(23);
+    expect(findFirstNotOf(s, '\0')).toEqual(0);
+  });
+  it('emoji', () => {
+    const s = '🍣🍺';
+    expect(findFirstNotOf(s, '🍣')).toEqual(1);
+    expect(findFirstNotOf(s, '👩‍❤️‍💋‍👨')).toEqual(0);
   });
 });
