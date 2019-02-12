ab682789e2e69e79323ade0c4a422a136524a283
yumetodo <yume-wikijp@live.jp>
2019-02-12 10:02:18 +0900

    feat(test): add findLastof test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { findFirstOf } from '../src';
+import { findFirstOf, findLastof } from '../src';
 
 describe('findFirstOf', () => {
   it('by string', () => {
@@ -20,5 +20,27 @@ describe('findFirstOf', () => {
     const s = '🍣🍺';
     expect(findFirstOf(s, '🍺')).toEqual(1);
     expect(findFirstOf(s, '👩‍❤️‍💋‍👨')).toEqual(-1);
+  });
+});
+describe('findLastOf', () => {
+  it('by string', () => {
+    const s = 'Hello, world. Welcome to C++ world.';
+    const str = 'world';
+    expect(findLastof(s, str)).toEqual(33);
+    expect(findLastof(s, str, 40)).toEqual(-1);
+    expect(findLastof(s, str, 25)).toEqual(23);
+    expect(findLastof(s, ',.+', 5)).toEqual(5);
+    expect(findLastof(s, 'ab')).toEqual(-1);
+  });
+  it('by char', () => {
+    const s = 'arikitari na sekai';
+    expect(findLastof(s, 'a')).toEqual(16);
+    expect(findLastof(s, 'a', 1)).toEqual(0);
+    expect(findLastof(s, '\0')).toEqual(-1);
+  });
+  it('emoji', () => {
+    const s = '🍣🍺';
+    expect(findLastof(s, '🍺')).toEqual(1);
+    expect(findLastof(s, '👩‍❤️‍💋‍👨')).toEqual(-1);
   });
 });
