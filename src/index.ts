2145da884479f086f15bef0cdefd553c5ce4328b
yumetodo <yume-wikijp@live.jp>
2019-05-31 20:40:25 +0900

    fix: ban to use string#substring

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
-type Pred = (c: string, k: string) => boolean;
-const findFirst = (target: string | string[], key: string, pos: number, pred: Pred, n?: number): number => {
-  const k = typeof n !== 'undefined' ? key.substring(0, n) : key;
+type Pred = (c: string, k: string | string[]) => boolean;
+const findFirst = (target: string | string[], key: string | string[], pos: number, pred: Pred): number => {
   let i = 0;
   for (const t of target) {
-    if (pos <= i && pred(t, k)) return i;
+    if (pos <= i && pred(t, key)) return i;
     ++i;
   }
   return -1;
+};
+const stringToArray = (s: string, n?: number): string | string[] => {
+  if (typeof n !== 'number') return s;
+  const it = s[Symbol.iterator]();
+  const re: string[] = [];
+  let i = 0;
+  while (true) {
+    if (i === n) break;
+    const ir = it.next();
+    if (ir.done) break;
+    re.push(ir.value);
+    ++i;
+  }
+  return re;
 };
 /**
  * Determines the lowest position `xpos`, if possible, such that both of the following conditions hold:
  *
  * 1. `pos <= xpos` and `xpos < size(target)`
- * 2. [`k.includes(at(target, xpos))`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes) (When `n` is `undefined` (omitted), `k` is equal to `key`. Otherwise, `k` is equal to [`key.substring(0, n)`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring))
+ * 2. [`k.includes(at(target, xpos))`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes) (When `n` is `undefined` (omitted), `k` is equal to `key`. Otherwise, `k` is equal to `std.substr(key, 0, n)`)
  *
  * @param target search target string
  * @param key string identifying characters to search for
@@ -21,20 +34,20 @@ const findFirst = (target: string | string[], key: string, pos: number, pred: Pr
  * @returns `xpos` if the function can determine such a value for `xpos`. Otherwise, returns `-1`.
  */
 export const findFirstOf = (target: string, key: string, pos = 0, n?: number): number =>
-  findFirst(target, key, pos, (c, k) => k.includes(c), n);
-const findLast = (target: string, key: string, pos: number, pred: Pred, n?: number): number => {
+  findFirst(target, stringToArray(key, n), pos, (c, k) => k.includes(c));
+const findLast = (target: string, key: string | string[], pos: number, pred: Pred): number => {
   const targetArr = Array.from(target);
   // if (targetArr.length <= pos) return -1;
   targetArr.reverse();
   pos = -1 === pos || targetArr.length <= pos ? 0 : targetArr.length - 1 - pos;
-  const re = findFirst(targetArr, key, pos, pred, n);
+  const re = findFirst(targetArr, key, pos, pred);
   return -1 === re ? -1 : targetArr.length - 1 - re;
 };
 /**
  * Determines the highest position `xpos`, if possible, such that both of the following conditions hold:
  *
  * 1. `pos <= xpos` and `xpos < size(target)`
- * 2. [`k.includes(at(target, xpos))`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes) (When `n` is `undefined` (omitted), `k` is equal to `key`. Otherwise, `k` is equal to [`key.substring(0, n)`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring))
+ * 2. [`k.includes(at(target, xpos))`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes) (When `n` is `undefined` (omitted), `k` is equal to `key`. Otherwise, `k` is equal to `std.substr(key, 0, n)`)
  *
  * @param target search target string
  * @param key string identifying characters to search for
@@ -43,12 +56,12 @@ const findLast = (target: string, key: string, pos: number, pred: Pred, n?: numb
  * @retunrs `xpos` if the function can determine such a value for `xpos`. Otherwise, returns `-1`.
  */
 export const findLastof = (target: string, key: string, pos = -1, n?: number): number =>
-  findLast(target, key, pos, (c, k) => k.includes(c), n);
+  findLast(target, stringToArray(key, n), pos, (c, k) => k.includes(c));
 /**
  * Determines the lowest position `xpos`, if possible, such that both of the following conditions hold:
  *
  * 1. `pos <= xpos` and `xpos < size(target)`
- * 2. [`!k.includes(at(target, xpos))`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes) (When `n` is `undefined` (omitted), `k` is equal to `key`. Otherwise, `k` is equal to [`key.substring(0, n)`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring))
+ * 2. [`!k.includes(at(target, xpos))`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes) (When `n` is `undefined` (omitted), `k` is equal to `key`. Otherwise, `k` is equal to `std.substr(key, 0, n)`)
  *
  * @param target search target string
  * @param key string identifying characters to search for
@@ -57,12 +70,12 @@ export const findLastof = (target: string, key: string, pos = -1, n?: number): n
  * @returns `xpos` if the function can determine such a value for `xpos`. Otherwise, returns `-1`.
  */
 export const findFirstNotOf = (target: string, key: string, pos = 0, n?: number): number =>
-  findFirst(target, key, pos, (c, k) => !k.includes(c), n);
+  findFirst(target, stringToArray(key, n), pos, (c, k) => !k.includes(c));
 /**
  * Determines the highest position `xpos`, if possible, such that both of the following conditions hold:
  *
  * 1. `pos <= xpos` and `xpos < size(target)`
- * 2. [`k.includes(at(target, xpos))`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes) (When `n` is `undefined` (omitted), `k` is equal to `key`. Otherwise, `k` is equal to [`key.substring(0, n)`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring))
+ * 2. [`k.includes(at(target, xpos))`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes) (When `n` is `undefined` (omitted), `k` is equal to `key`. Otherwise, `k` is equal to `std.substr(key, 0, n)`)
  *
  * @param target search target string
  * @param key string identifying characters to search for
@@ -71,4 +84,4 @@ export const findFirstNotOf = (target: string, key: string, pos = 0, n?: number)
  * @returns `xpos` if the function can determine such a value for `xpos`. Otherwise, returns `-1`.
  */
 export const findLastNotof = (target: string, key: string, pos = -1, n?: number): number =>
-  findLast(target, key, pos, (c, k) => !k.includes(c), n);
+  findLast(target, stringToArray(key, n), pos, (c, k) => !k.includes(c));
