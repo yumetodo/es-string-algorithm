f020ef43512b259656734050d93be4169f2ce0f1
yumetodo <yume-wikijp@live.jp>
2020-02-11 13:26:55 +0900

    chore: add license notice to tests ported from libcxx

--- test/rfind/basic.test.ts
+++ test/rfind/basic.test.ts
@@ -1,3 +1,10 @@
+//===----------------------------------------------------------------------===//
+//
+// Port from the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 import { rfind } from '../../src';
 describe('rfind', () => {
   it('basic', () => {
